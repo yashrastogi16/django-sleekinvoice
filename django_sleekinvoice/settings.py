bc1d83702747d2d29e332ababd17056b0beabde6
yashrastogi16 <yash.rastogi916@gmail.com>
2016-12-07 15:04:46 +0530

    added company and invoice seperate app, completed the models for company

--- django_sleekinvoice/settings.py
+++ django_sleekinvoice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'company',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'django_sleekinvoice.urls'
+AUTH_USER_MODEL = 'users.User'
 
 TEMPLATES = [
     {
--- django_sleekinvoice/urls.py
+++ django_sleekinvoice/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from base.admin import admin_site
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    # Site Manager
+    url(r'^admin/', admin_site.urls),
+    ]
